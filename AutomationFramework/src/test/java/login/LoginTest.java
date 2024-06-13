8be1d57357d1df09af9e634d864e0b6804676f9f
user lenovo <user lenovo@DESKTOP-H2J9PSJ>
2024-06-13 08:57:58 +0530

    Test class

---
+++ AutomationFramework/src/test/java/login/LoginTest.java
@@ -0,0 +1,14 @@
+package login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LoginTest 
+{
+	@Test
+	public void test()
+	{
+		Reporter.log("Test calss added", true);
+	}
+
+}
